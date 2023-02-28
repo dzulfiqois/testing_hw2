4b77e6b0243704a382f0710716b8f6b4f5d4ac42
dzulfiqois <qdn.bd2104@gmail.com>
2023-02-28 09:06:58 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print("Hello World!");
+
+  runApp(MaterialApp(
+      home: Scaffold(
+          body: Center(
+              child: Text(
+                  "Hello World!\nyowasapmafren\nQois Dzulfikar Nugroho.")))));
+}
